988566eadea85ae8f91473700c1e0265f7dfb5d4
Vinicius Yukio Ishikawa <viniyishikawa@gmail.com>
2026-02-17 15:50:22 +0000

    FEAT: Created contact and location section structure

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +12,10 @@ import {MatIconModule} from '@angular/material/icon';
   imports: [
     MatButtonModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
