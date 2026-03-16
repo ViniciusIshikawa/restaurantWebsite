55f3c40949bb11ad830d583445ac501dccda9c4d
Vinicius Yukio Ishikawa <viniyishikawa@gmail.com>
2026-03-16 00:35:20 +0000

    Style: Added maps and reactive form for the contact form on the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,8 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { MatDivider } from "@angular/material/divider";
+import * as Leaflet from 'leaflet';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -19,19 +21,43 @@ import { MatDivider } from "@angular/material/divider";
     MatSelectModule,
     MatInputModule,
     RouterLink,
-    MatDivider
+    MatDivider,
+    ReactiveFormsModule
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-  constructor(private route: ActivatedRoute){}
+  contactForm!: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ){}
 
   ngOnInit(): void {
     window.scrollTo({top: 0, behavior: 'smooth'});
+
+    this.contactForm = this.fb.group({
+      name: [[''], [Validators.required]],
+      email: [[''], [Validators.required, Validators.email]],
+      phoneNumber: [[''], [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+      subject: [[''], [Validators.required]],
+      more: ['']
+    })
   }
 
   ngAfterViewInit() {
+    const map = Leaflet.map('map').setView([53.3498, -6.2603], 13);
+
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    Leaflet.marker([53.3498, -6.2603]).addTo(map)
+      .openPopup();
+
     this.route.fragment.subscribe(f => {
       if (f) {
         document.getElementById(f)?.scrollIntoView({ behavior: 'smooth' });
